c6b7e9008a66a4bc89647d9de1065e6c8707f7e7
rmmarcos <r.marcos.1980@gmail.com>
2022-01-11 19:26:36 +0100

    Quitar token

--- funciones.R
+++ funciones.R
@@ -9,7 +9,5 @@ ndiff <- function(b1,b2){
 nsavi <- function(b1,b2,L=0.5){
   return((b1-b2)/(b1+b2+L)*(1+L))
 }
-=======
-############
-#Crear una funcion con nSAVI
->>>>>>> d78b0a7b1a4734f41547817bf15b632c542fc379
+
+
